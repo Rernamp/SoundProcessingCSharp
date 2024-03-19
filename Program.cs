a42d38199b0bed48c9e594884ac11d15ad928135
rernamp <renamp100@gmail.com>
2024-03-19 20:04:17 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class Program {
+    public static void Main(string[] args) {
+        // create a plot
+        ScottPlot.Plot myPlot = new();
+        myPlot.Add.Signal(ScottPlot.Generate.Sin());
+        myPlot.Add.Signal(ScottPlot.Generate.Cos());
+
+        // launch the plot in an interactive plot window
+        ScottPlot.WinForms.FormsPlotViewer.Launch(myPlot);
+        ScottPlot.WinForms.FormsPlotViewer.Launch(myPlot, "12");
+    }
+}
