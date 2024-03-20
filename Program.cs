8a93b4c95863dbfce32b2d7c15210f261c6d1a00
rernamp <renamp100@gmail.com>
2024-03-20 22:45:30 +0300

    add load data from wav file and plot it on another figure

--- Program.cs
+++ Program.cs
@@ -1,14 +1,54 @@
-﻿
+﻿using System;
+using System.Threading;
+using NWaves;
+using NWaves.Audio;
+using NWaves.Signals;
+using SoundProcessing;
 
 public class Program {
     public static void Main(string[] args) {
-        // create a plot
-        ScottPlot.Plot myPlot = new();
-        myPlot.Add.Signal(ScottPlot.Generate.Sin());
-        myPlot.Add.Signal(ScottPlot.Generate.Cos());
+        if (args.Count() != 2) {
+            Console.WriteLine("Invalid number arguments");
+        }
+        string soundFilePath = args[0];
+        WaveFile soundFile = Utils.LoadFile(soundFilePath);
 
-        // launch the plot in an interactive plot window
-        ScottPlot.WinForms.FormsPlotViewer.Launch(myPlot);
-        ScottPlot.WinForms.FormsPlotViewer.Launch(myPlot, "12");
+        string noiseFilePath = args[1];
+        WaveFile noiseFile = Utils.LoadFile(noiseFilePath);
+
+        Console.WriteLine($"Load file {soundFilePath}");
+        Utils.PrintFileInfo(soundFile);
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"Load file {noiseFilePath}");
+        Utils.PrintFileInfo(noiseFile);
+
+
+        DiscreteSignal sound = soundFile[0];
+        DiscreteSignal noise = noiseFile[0];
+
+        if (noise.Length > sound.Length) {
+            noise = noise.First(sound.Length);
+        } else {
+            sound = sound.First(noise.Length);
+        }
+
+        ScottPlot.Plot soundPlot = new();
+        soundPlot.Add.Signal(sound.Samples, 1 / (float)sound.SamplingRate);
+
+        ScottPlot.Plot noisePlot = new();
+        noisePlot.Add.Signal(noise.Samples, 1 / (float)noise.SamplingRate);
+
+        new Thread(() => ScottPlot.WinForms.FormsPlotViewer.Launch(soundPlot, "Sound signal")).Start();
+        new Thread(() => ScottPlot.WinForms.FormsPlotViewer.Launch(noisePlot, "Noise signal")).Start();
+
+        DiscreteSignal soundWithNoise = sound + noise;
+
+        ScottPlot.Plot soundWithNoisePlot = new();
+        soundWithNoisePlot.Add.Signal(soundWithNoise.Samples, 1 / (float)soundWithNoise.SamplingRate);
+
+        new Thread(() => ScottPlot.WinForms.FormsPlotViewer.Launch(soundWithNoisePlot, "Sound with noise")).Start();
+
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿using NWaves;
+using NWaves.Audio;
+using ScottPlot.Plottables;
+
+namespace SoundProcessing {
+    internal class Utils {
+        public static WaveFile LoadFile(string filePath) {
+            WaveFile waveFile;
+
+            if (System.IO.Path.GetExtension(filePath) != ".wav") {
+                throw new ArgumentException($"Don`t supported extension file: {filePath}. Supported only wav file");
+            }
+            using (var stream = new FileStream(filePath, FileMode.Open)) {
+                waveFile = new WaveFile(stream);
+            }
+
+            return waveFile;
+        }
+
+        public static void PrintFileInfo(WaveFile file) {
+            Console.WriteLine($"ChannelCount: {file.WaveFmt.ChannelCount}");
+            Console.WriteLine($"SamplingRate : {file.WaveFmt.SamplingRate}");
+            Console.WriteLine($"SampleCount: {file.Signals[0].Length}");
+        }
+    }
+}
